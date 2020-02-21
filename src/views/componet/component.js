43f81cf0ada225ce6899c9733377db8cc20ef47f
ConstanzaCastro <redlight.down@gmail.com>
2020-02-21 15:03:09 -0300

    avance

--- src/views/componet/component.js
+++ src/views/componet/component.js
@@ -4,6 +4,27 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+class Name extends React.Component {
+    constructor(props) {
+
+        super(props);
+        this.handleThis = this...bind(this); //faltan variables
+    }
+
+    render() {
+        return (
+            /*<form>
+           <label>
+            Nombre:
+           <input type="text" name="name" />
+           </label>
+           <input type="submit" value="Submit" />
+           </form>*/
+        );
+    }
+}
+
+
 /*<form>
   <label>
     Nombre:
